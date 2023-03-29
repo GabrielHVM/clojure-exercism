4d1b595fcbc6b1dde11537de63b0773275e6230b
gabriel.mata <gabriel.mata@nubank.com.br>
2023-03-29 19:29:28 -0300

    Adds Accumulate

--- exercises/practice/accumulate/src/accumulate.clj
+++ exercises/practice/accumulate/src/accumulate.clj
@@ -1,5 +1,17 @@
 (ns accumulate)
 
-(defn accumulate [] ;; <- arglist goes here
-      ;; your code goes here
-)
+#_(defn accumulate [function coll]
+        (reduce (fn [new-coll item-coll]
+                    (->> item-coll
+                         function
+                         (conj new-coll))) [] coll))
+
+(defn accumulate
+      ([function coll]
+       (accumulate [] function coll))
+      ([new-coll function coll]
+       (if (empty? coll)
+         new-coll
+         (let [first-item (first coll)
+               coll-with-new-item (conj new-coll (function first-item))]
+              (recur coll-with-new-item function (rest coll))))))
