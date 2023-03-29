84fde640fdb05f86ffaef2bb19c26825d31b53b3
gabriel.mata <gabriel.mata@nubank.com.br>
2023-03-29 19:30:29 -0300

    Adds Elyses Destructured Enchantments

--- exercises/concept/elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
+++ exercises/concept/elyses-destructured-enchantments/src/elyses_destructured_enchantments.clj
@@ -1,30 +1,40 @@
 (ns elyses-destructured-enchantments)
 
 (defn first-card
-  "Returns the first card from deck."
-  [deck]
-)
+      "Returns the first card from deck."
+      [[first-card-of-deck]]
+      first-card-of-deck)
 
 (defn second-card
-  "Returns the second card from deck."
-  [deck]
-)
+      "Returns the second card from deck."
+      [[_ second-card-of-deck]]
+      second-card-of-deck)
 
 (defn swap-top-two-cards
-  "Returns the deck with first two items reversed."
-  [deck]
-)
+      "Returns the deck with first two items reversed."
+      [[first-card second-card & rest]]
+      (-> rest
+          (conj first-card)
+          (conj second-card)
+          vec))
 
 (defn discard-top-card
-  "Returns a sequence containing the first card and
-   a sequence of the remaining cards in the deck."
-  [deck]
-)
+      "Returns a sequence containing the first card and
+       a sequence of the remaining cards in the deck."
+      [[top-card-deck & rest]]
+      (->> (and rest (vec rest))
+           (conj [] top-card-deck)))
 
 (def face-cards
   ["jack" "queen" "king"])
 
 (defn insert-face-cards
-  "Returns the deck with face cards between its head and tail."
-  [deck]
-)
+      "Returns the deck with face cards between its head and tail."
+      [[first-card & rest-of-deck :as deck]]
+      (let [add-face-cards #(apply conj % (reverse face-cards))]
+           (if (empty? deck)
+             (vec (add-face-cards '()))
+             (-> (or rest-of-deck '())
+                 add-face-cards
+                 (conj first-card)
+                 vec))))
