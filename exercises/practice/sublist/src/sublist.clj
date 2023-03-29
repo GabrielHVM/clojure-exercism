18991c318714f9d7c321c8f18fe03c3ffc38cd8e
gabriel.mata <gabriel.mata@nubank.com.br>
2023-03-29 19:31:52 -0300

    Adds Sublist

--- exercises/practice/sublist/src/sublist.clj
+++ exercises/practice/sublist/src/sublist.clj
@@ -1,5 +1,14 @@
 (ns sublist)
 
-(defn classify [list1 list2] ;; <- arglist goes here
-      ;; your code goes here
-)
+(defn is-sublist?
+      [list1 list2]
+      ;; a step smaller than the partition-size results in reuse on partition function.
+      (some #{list1} (partition (count list1) 1 list2)))
+
+(defn classify
+      [list1 list2]
+      (cond
+       (= list1 list2) :equal
+       (is-sublist? list1 list2) :sublist
+       (is-sublist? list2 list1) :superlist
+       :else :unequal))
